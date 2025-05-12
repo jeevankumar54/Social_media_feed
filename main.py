4c5a617333e19c201e9f178e8bf9be4587828080
jeevankumar54 <tjeevankumar54@gmail.com>
2025-05-12 13:44:30 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from typing import List, Optional
+from db import users
+
+class Notification(BaseModel):
+    author: str
+    description: str
+
+class User(BaseModel):
+    name: str
+    username: str
+    email: str
+    birthday: str
+    friends: str
+    notifications: List[Notification]
+
+class UserDb(User):
+    hashed_password: str
+    
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def root():
+    return {"Set": "up!"}
