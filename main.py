9afe4268e4726555c3b25938ee9b209fa680f14e
jeevankumar54 <tjeevankumar54@gmail.com>
2025-05-12 14:54:40 +0530

    Creating base home page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -20,11 +20,11 @@ class User(BaseModel):
 
 class UserDb(User):
     hashed_password: str
-    
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def root():
-    return {"Set": "up!"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "FriendConnect - Home"})
